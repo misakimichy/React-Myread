c06dfbdccbce42f4a5a716484b990f8a50580503
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-03 20:06:05 -0700

    Add SearchWindow component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 // import * as BooksAPI from './BooksAPI';
 import './App.css';
+// import BookList from './BookList';
+import SearchWindow from './SearchWindow';
+
 
 class BooksApp extends React.Component {
   state = {
+    books:[],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -11,32 +15,16 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false
-  }
+  };
 
   render() {
+    const { books } = this.state;
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+        {/* <BookList /> */}
+        <SearchWindow books={books} />
+  
+        {/*   
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -197,7 +185,7 @@ class BooksApp extends React.Component {
               <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
             </div>
           </div>
-        )}
+        )} */}
       </div>
     )
   }
