1fcd129fdf11747c94caf8803def2efb38f30ed9
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-03 21:48:28 -0700

    Feat: Show message when the book is added to the shelf and if it's failed show alert message.

--- src/SearchWindow.js
+++ src/SearchWindow.js
@@ -8,11 +8,21 @@ class SearchWindow extends Component {
         showBooks: [],
     };
 
+    // Update query
     handleQueryUpdate = query => {
         BooksAPI.search(query)
-        .then(books => books ? 
-        this.setState({ books })
-        : []);
+            .then(books => books ?
+                this.setState({ books })
+                : []);
+                this.setState({ query });
+    }
+
+    // Show message when the book is successfully added to the shelf.
+    // 'None' should be selected if a book has not been assigned to a shelf.
+    handleBookShelf(book, shelf) {
+        BooksAPI.update(book, shelf)
+            .then(() => shelf !== 'none' ? alert(`We added ${book.title} to your bookshelf.`) : '')
+            .catch(() => alert('Please try again!'));
     }
 
     render() {
@@ -30,6 +40,8 @@ class SearchWindow extends Component {
                         <input 
                             type="text" 
                             placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={this.handleQueryUpdate}
                         />
                     </div>
                 </div>
