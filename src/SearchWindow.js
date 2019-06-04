49f92e6e677ec52b44a017ce7ec8d97e0cb81bc1
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-03 21:34:18 -0700

    Import BookAPI.

--- src/SearchWindow.js
+++ src/SearchWindow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 
 class SearchWindow extends Component {
     state = {
@@ -7,9 +8,11 @@ class SearchWindow extends Component {
         showBooks: [],
     };
 
-    getBooks = event => {
-        const query = event.target.value;
-        this.setState({ query });
+    handleQueryUpdate = query => {
+        BooksAPI.search(query)
+        .then(books => books ? 
+        this.setState({ books })
+        : []);
     }
 
     render() {
@@ -18,20 +21,12 @@ class SearchWindow extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <button 
-                        className="close-search" 
-                        onClick={() => this.setState({ showSearchPage: false })}
+                    <Link
+                        to='/' 
+                        className='close-search'
                     >Close
-                    </button>
+                    </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
                         <input 
                             type="text" 
                             placeholder="Search by title or author"
