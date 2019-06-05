19db504f6d52c21a292fb460bf28ada595be4476
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-05 14:11:13 -0700

    Feat: Add prop-types.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import BookListDetail from './BookListDetail';
 
-const BookShelf = ({title, books, handleShelf}) => {
+const BookShelf = ({ title, books, updateBookShelf }) => {
     return(
         <div className="bookshelf">
             <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {books.map((book, index) =>
-                        <BookListDetail
-                            key={index}
-                            book={book}
-                            handleShelf={handleShelf}
-                        />
-                    )}
+                    <BookListDetail
+                        books={books}
+                        updateBookShelf={updateBookShelf}
+                    />
                 </ol>
             </div>
         </div>
     );
+};
 
+// Add prop-types
+BookShelf.propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    updateBookShelf: PropTypes.func.isRequired
 };
 
 export default BookShelf;
