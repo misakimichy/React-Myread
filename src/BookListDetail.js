03ee080f4fd0dc6b1132c7a7f2eaddcea41937f8
Misaki Koonce <misaki.koonce@gmail.com>
2019-06-05 10:14:24 -0700

    Style: Fix indent of JSX.

--- src/BookListDetail.js
+++ src/BookListDetail.js
@@ -5,29 +5,29 @@ const BookListDetail = ({ book, handleShelf }) => {
 
     return (
         <li>
-          <div className="book">
-            <div className="book-top">
-              <div 
-                className="book-cover" 
-                style={{ width: 128, height: 193, backgroundImage: `url(${imageThumbnails})` }}
-                >
-            </div>
-                <div className="book-shelf-changer">
-                    <select 
-                        onChange={event => handleShelf(book, event.target.value)} 
-                        value={book.shelf}
+            <div className="book">
+                <div className="book-top">
+                    <div
+                        className="book-cover"
+                        style={{ width: 128, height: 193, backgroundImage: `url(${imageThumbnails})` }}
                     >
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
+                    </div>
+                    <div className="book-shelf-changer">
+                        <select
+                            onChange={event => handleShelf(book, event.target.value)}
+                            value={book.shelf}
+                        >
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
                         </select>
                     </div>
                 </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
-          </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+            </div>
         </li>
     );
 };
