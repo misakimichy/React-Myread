012b36ba5e9743e226f68f442481d8e146900254
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-13 16:04:20 -0800

    Update format.

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,18 +1,16 @@
-
 const api = "https://reactnd-books-api.udacity.com"
-
 
 // Generate a unique token for storing your bookshelf data on the backend server.
 let token = localStorage.token
 if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+  token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
   'Accept': 'application/json',
   'Authorization': token
 }
 
-export const get = (bookId) =>
+export const get = bookId =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then(res => res.json())
     .then(data => data.book)
@@ -32,7 +30,7 @@ export const update = (book, shelf) =>
     body: JSON.stringify({ shelf })
   }).then(res => res.json())
 
-export const search = (query) =>
+export const search = query =>
   fetch(`${api}/search`, {
     method: 'POST',
     headers: {
