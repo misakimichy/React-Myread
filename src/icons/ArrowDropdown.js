06c52a8b6f376c17d284462daa24719938d6d575
Misaki Koonce <misaki.koonce@gmail.com>
2020-09-25 13:21:12 -0700

    Updates

--- src/icons/ArrowDropdown.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ArrowDropdown = () => (
-  <svg fill="#2b4162" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path d="M7 10l5 5 5-5z"/>
-  </svg>
-);
-
-export default ArrowDropdown;
